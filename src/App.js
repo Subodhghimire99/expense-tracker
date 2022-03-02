766979dec5c24bae000235052c8ca77ddcd3f7f4
subodhghimire99 <subodhghimire99@gmail.com>
2022-03-02 22:50:07 +0530

    Updated components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import MyData from "./components/MyData";
+
+let myObj = {
+  Name: "Subodh",
+  Age: 20,
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My Data</h2>
+      <MyData data={myObj} />
     </div>
   );
 }
---
+++ src/components/MyData.js
@@ -0,0 +1,10 @@
+function MyData(props) {
+  return (
+    <div>
+      <h2>{props.data.Name}</h2>
+      <h4>{props.data.Age}</h4>
+    </div>
+  );
+}
+
+export default MyData;
