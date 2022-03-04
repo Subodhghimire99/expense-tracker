29c8f8aaf32bc85d95e0e055fa617402422d5f73
subodhghimire99 <subodhghimire99@gmail.com>
2022-03-05 00:27:52 +0530

    ExpenseDate component added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import "./App.css";
 
 function App() {
   const expenses = [
-    { title: "Car Insurance", amount: 24500, date: new Date(2022, 1, 22) },
+    { title: "Car Insurance", amount: 24500, date: new Date(2022, 0, 22) },
     {
       title: "Birthday Celebration",
       amount: 10000,
-      date: new Date(2022, 3, 22),
+      date: new Date(2022, 2, 22),
     },
     {
       title: "Mart Shopping",
       amount: 5000,
-      date: new Date(2022, 5, 22),
+      date: new Date(2022, 4, 22),
     },
     {
       title: "Flat rent",
       amount: 5000,
-      date: new Date(2022, 4, 2),
+      date: new Date(2022, 5, 2),
     },
   ];
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <div className="expense-item__date">
-        <h2>{props.date.toISOString()}</h2>
+        <ExpenseDate date={props.date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
