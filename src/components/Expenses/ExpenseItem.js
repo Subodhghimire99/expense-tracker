11392e37e8886da3bdfa4e7fee371258ab0a035e
subodhghimire99 <subodhghimire99@gmail.com>
2022-03-08 23:33:14 +0530

    state management integrated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setTitle("None");
+    setAmount(0);
+  };
+
   return (
     <Card className="expense-item">
       <div className="expense-item__date">
         <ExpenseDate date={props.date}></ExpenseDate>
       </div>
-      <Card className="expense-item__description">{props.title}</Card>
-      <Card className="expense-item__price">Rs{props.amount}</Card>
+      <Card className="expense-item__description">{title}</Card>
+      <Card className="expense-item__price">Rs{amount}</Card>
+      <button onClick={clickHandler}>Reset</button>
     </Card>
   );
 };
